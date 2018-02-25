aee7e7098221beff2ede2b6f70dacb5f44e8bfed
grapeperson <grapeperson@inbox.ru>
2018-02-25 13:03:05 +0300

    feature/add method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let zeros = 0;
+    for (let i = 5; number/i >= 1; i *= 5)
+      zeros += Math.floor(number/i);
+    return zeros;
 }
